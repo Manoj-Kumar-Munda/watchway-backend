62ef3f6996b8638927f3c48fbb5a4967b6fc1b39
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-05-16 11:33:42 +0530

    all comments controller done

--- src/app.js
+++ src/app.js
@@ -22,12 +22,14 @@ import videoRouter from "./routes/video.router.js";
 import playlistRouter from "./routes/playlist.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import likeRouter from "./routes/like.routes.js";
+import commentRouter from "./routes/comment.routes.js";
 //routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/playlist", playlistRouter);
 app.use("/api/v1/subscription", subscriptionRouter);
 app.use("/api/v1/like", likeRouter);
+app.use("/api/v1/comments", commentRouter);
 
 
 app.use((err, req, res, next) => {
--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,31 +1,154 @@
-import mongoose from "mongoose"
-import {Comment} from "../models/comment.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, { isValidObjectId } from "mongoose";
+import { Comment } from "../models/comment.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getVideoComments = asyncHandler(async (req, res) => {
-    //TODO: get all comments for a video
-    const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
+  const { videoId } = req.params;
+  let { page = 1, limit = 10 } = req.query;
 
-})
+  page = (parseInt(page) && (page < 1 ? 1 : page)) || 1;
+  limit = (parseInt(limit) && (limit < 1 ? 10 : limit)) || 10;
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid videoId");
+  }
+
+  const aggregate = Comment.aggregate(
+    [
+        {
+          $match: {
+            video: new mongoose.Types.ObjectId(videoId)
+          }
+        },
+        {
+          $lookup: {
+            from: "users",
+            localField: "owner",
+            foreignField: "_id",
+            as: "owner"
+          }
+        },
+        {
+          $unwind: {
+            path: '$owner'
+          }
+        },
+        {
+          $lookup: {
+            from: "likes",
+            localField: "_id",
+            foreignField: "comment",
+            as: "likes"
+          }
+        },
+        {
+          $addFields: {
+            totalLikes: {
+              $size: "$likes"
+            },
+            isLiked: {
+              $cond: {
+                if: {
+                  $in: [req.user._id, "$likes.likedBy"]
+                },
+                then : true,
+                else: false
+              }
+            }
+          }
+        },
+        {
+          $project: {
+            content: 1,
+            createdAt: 1,
+            owner: {
+              username: 1,
+              fullName: 1,
+              avatar: 1
+            },
+            totalLikes: 1,
+            isLiked: 1
+          }
+        }
+      ]
+  );
+
+  const comments = await Comment.aggregatePaginate(aggregate, {
+    page,
+    limit,
+  });
+
+  return res.status(200).json(new ApiResponse(200, comments));
+});
 
 const addComment = asyncHandler(async (req, res) => {
-    // TODO: add a comment to a video
-})
+  const { videoId } = req.params;
+  const { content } = req.body;
+
+  if (!content.trim()) {
+    throw new ApiError(400, "Comment is required");
+  }
+
+  const comment = await Comment.create({
+    video: videoId,
+    content,
+    owner: req.user?._id,
+  });
+
+  if (!comment) {
+    throw new ApiError(500, "Failed to add your comment");
+  }
+
+  return res.status(200).json(new ApiResponse(200, comment, "Comment added"));
+});
 
 const updateComment = asyncHandler(async (req, res) => {
-    // TODO: update a comment
-})
+  const { commentId } = req.params;
+  const { content } = req.body;
+
+  if (!content) {
+    throw new ApiError(400, "Content is required");
+  }
+
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, "commentId is not valid");
+  }
+
+  const updatedComment = await Comment.findByIdAndUpdate(
+    commentId,
+    {
+      content,
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!updatedComment) {
+    throw new ApiError(500, "Failed to update comment");
+  }
+
+  return res.status(200).json(new ApiResponse(200, updatedComment));
+});
 
 const deleteComment = asyncHandler(async (req, res) => {
-    // TODO: delete a comment
-})
+  const { commentId } = req.params;
 
-export {
-    getVideoComments, 
-    addComment, 
-    updateComment,
-     deleteComment
-    }
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, "Invalid commentId");
+  }
+
+  const deletedComment = await Comment.findByIdAndDelete(commentId);
+
+  if (!deletedComment) {
+    throw new ApiError(500, "Failed to delete comment");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Comment deleted successfully"));
+});
+
+export { getVideoComments, addComment, updateComment, deleteComment };
