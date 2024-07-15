fad57400ae745d5053cd63a770bbd839d4d6e706
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-07-15 22:55:50 +0530

    cors issue fixed

--- src/app.js
+++ src/app.js
@@ -3,10 +3,9 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
-
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: "*",
     credentials: true,
   })
 );
