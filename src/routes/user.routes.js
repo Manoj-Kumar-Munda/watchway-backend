f3d866e4d680cbf9ecd2fcd032a2e1ca6783ad18
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-07-22 21:35:42 +0530

    removed required auth for getUserChannelProfile

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -47,7 +47,7 @@ router
   .route("/cover-image")
   .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
-router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/channel/:username").get( getUserChannelProfile);
 router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
